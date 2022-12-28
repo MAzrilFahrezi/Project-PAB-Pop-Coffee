45c73af50bf82fd8935f8c5c20f2fb83ad25f355
Fadhilsaadat <fadhilsa.adat16@gmail.com>
2022-12-28 16:58:15 +0700

    add layout detail

---
+++ app/src/main/java/com/popguy/projectpabpopcoffee/activites/activity_detailkopi.java
@@ -0,0 +1,16 @@
+package com.popguy.projectpabpopcoffee.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.popguy.projectpabpopcoffee.R;
+
+public class activity_detailkopi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailkopi);
+    }
+}
