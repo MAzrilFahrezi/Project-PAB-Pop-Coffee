083aa4363684920d7596c7c54eec7e6e72075744
AzrilFahrezi <mazrilfahrezi@mhs.mdp.ac.id>
2022-12-28 21:51:13 +0700

    update user activity

--- app/src/main/java/com/popguy/projectpabpopcoffee/activites/User_Activity.java
+++ app/src/main/java/com/popguy/projectpabpopcoffee/activites/User_Activity.java
@@ -1,5 +1,6 @@
 package com.popguy.projectpabpopcoffee.activites;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,12 +10,19 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.popguy.projectpabpopcoffee.R;
 import com.popguy.projectpabpopcoffee.databinding.ActivityUserBinding;
 
 public class User_Activity extends AppCompatActivity {
     private ActivityUserBinding binding;
     FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    String UserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,20 @@ public class User_Activity extends AppCompatActivity {
             }
         });
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        UserId = firebaseAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = firebaseFirestore.collection("User").document(UserId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                binding.tvNamaUser.setText(value.getString("nama"));
+                binding.tvEmailuser.setText(value.getString("email"));
+                binding.tvNohp.setText(value.getString("noHp"));
+            }
+        });
     }
 
 
