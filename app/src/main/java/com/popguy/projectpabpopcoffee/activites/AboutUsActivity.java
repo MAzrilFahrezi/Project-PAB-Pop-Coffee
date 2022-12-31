8c3818d0527e8fff9ccae66deaec2539d3799ca5
kelvinwahyudi <powpoww69@gmail.com>
2022-12-31 16:19:05 +0700

    java

--- app/src/main/java/com/popguy/projectpabpopcoffee/activites/AboutUsActivity.java
+++ app/src/main/java/com/popguy/projectpabpopcoffee/activites/AboutUsActivity.java
@@ -1,14 +1,83 @@
 package com.popguy.projectpabpopcoffee.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.popguy.projectpabpopcoffee.databinding.ActivityAboutUsBinding;
 
 public class AboutUsActivity extends AppCompatActivity {
+private ActivityAboutUsBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
+        binding = ActivityAboutUsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.cvpadel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Uri uri = Uri.parse("https://www.instagram.com/fadhilsa.adat/");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.instagram.com/fadhilsa.adat/")));
+
+                }
+            }
+        });
+
+        binding.cvkelvin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Uri uri = Uri.parse("https://www.instagram.com/kelvinwxydi/");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.instagram.com/kelvinwxydi/")));
+
+                }
+            }
+
+        });
+
+        binding.cvzey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.instagram.com/azrlfhrzi/");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.instagram.com/azrlfhrzi/")));
+
+                }
+
+            }
+        });
+
     }
 }
