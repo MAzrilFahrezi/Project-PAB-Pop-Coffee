ba1f90a7e92279b6dfa414a752a7a2800c67a96c
kelvinwahyudi <powpoww69@gmail.com>
2023-01-02 21:52:51 +0700

    fix detail

--- app/src/main/java/com/popguy/projectpabpopcoffee/activites/activity_detailkopi.java
+++ app/src/main/java/com/popguy/projectpabpopcoffee/activites/activity_detailkopi.java
@@ -3,6 +3,7 @@ package com.popguy.projectpabpopcoffee.activites;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.popguy.projectpabpopcoffee.R;
 import com.popguy.projectpabpopcoffee.databinding.ActivityDetailkopiBinding;
@@ -11,12 +12,17 @@ public class activity_detailkopi extends AppCompatActivity {
 
     private ActivityDetailkopiBinding binding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityDetailkopiBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.ivBackk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { onBackPressed(); }
+        });
 
         String namaKopi = getIntent().getStringExtra("nama_kopi");
         String asalKopi = getIntent().getStringExtra("asal_kopi");
